11d98b4485e1548252482b6bf76e1e81c8ec9d9d
divyasrichindam <divyasrichindam02@gmail.com>
2018-07-23 12:27:32 -0400

    Adding the project

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class ErrorBoundary extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			hasError: False
+		}
+		
+	}
+	
+}
+
+export default ErrorBoundary;
